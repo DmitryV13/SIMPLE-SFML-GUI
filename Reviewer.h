90d2f63b034aeee5c894a3cc110d65bf2c5f2fe8
Dmitriy Voicu <voiku173dmitry@gmail.com>
2024-04-07 07:41:10 +0300

    were added other classes' description

--- Reviewer.h
+++ Reviewer.h
@@ -5,6 +5,9 @@
 
 using namespace::sf;
 
+/**
+ * Представляет галерею картинок.
+ */
 class Reviewer : public InterfaceItem{
 private:
 	short current_image;
@@ -14,25 +17,92 @@ private:
 	float max_height;
 	Vector2f position;
 public:
+    /**
+
+        @param x координата.
+        @param y координата.
+        @param width_ ширина.
+        @param height_ высота.
+    */
 	Reviewer(float x, float y, float width_, float height_);
 	~Reviewer();
 
 	FloatRect getLocalBounds() override;
 	FloatRect getGlobalBounds() override;
 
+    /** Добавление в галлерею анимированной картинки.
+
+        @param t_manager менеджер текстур.
+        @param index индекс строки содержащей данную текстуру.
+        @param name название текстуры, сохраненной в менеджере текстур.
+        @param first_frame координаты и размеры части текстуры, которая будет отображаться первой.
+        @param frames_number_ количество кадров в данной текстуре.
+    */
 	void addAnimatedImage(TextureManager* t_manager, int index, string name, IntRect first_frame, short frames_number_);
+
+    /** Добавление в галлерею анимированной картинки.
+
+        @param t_manager менеджер текстур.
+        @param w_object обьект склада информации о сущностях.
+        @param name название текстуры, сохраненной в менеджере текстур.
+    */
 	void addAnimatedImage(TextureManager* t_manager, Warehouse* w_object, string name);
+
+    /** Добавление в галлерею неанимированной картинки.
+
+        @param t_manager менеджер текстур.
+        @param index индекс строки содержащей данную текстуру.
+        @param name название текстуры, сохраненной в менеджере текстур.
+        @param first_frame координаты и размеры части текстуры, которая будет отображаться первой.
+    */
 	void addStaticImage(TextureManager* t_manager, int index, string name, IntRect first_frame);
+
+    /** Добавление в галлерею неанимированной картинки.
+
+        @param t_manager менеджер текстур.
+        @param w_object обьект склада информации о сущностях.
+        @param name название текстуры, сохраненной в менеджере текстур.
+    */
 	void addStaticImage(TextureManager* t_manager, Warehouse* w_object, string name);
+
+    /** Устанавливает новую позицию.
+
+       @param position_ координаты новой позиции.
+    */
 	void setPosition(Vector2f position_);
+
+    /** Устанавливает новую координату x для группы. Провоцирует изменение положения всех элементов группы.
+
+       @param x новая координата x.
+    */
 	void setPositionX(float x) override;
+
+    /** Устанавливает новую координату y для группы. Провоцирует изменение положения всех элементов группы.
+
+       @param y новая координата y.
+    */
 	void setPositionY(float y) override;
 
+    /** Сдвигает группу и ее элементы на заданное смещение.
+
+       @param offset_x смещение по x.
+       @param offset_y смещение по y.
+    */
 	void changePosition(float offset_x, float offset_y) override;
 
+    /** Изменяет картинку, которая отображается в данный момент.
+
+       @param next отвечает за то, какая картинка после текущей по счету будет отображаться.
+       @param param2 параметр заглушка (не используется).
+    */
 	void changeCurrentImage(float next, float param2);
 
+    /** Обновляет элемент.
+    */
 	void update() override;
+
+    /** Отображает элемент.
+    */
 	void render() override;
 };
 
--- ScaleParametrBar.h
+++ ScaleParametrBar.h
@@ -6,6 +6,9 @@
 
 using namespace sf;
 
+/** Элемент интерфейса - шкала значения ресурса.
+
+ */
 class ScaleParameterBar : public InterfaceItem{
 private:
 	Sprite scale_bar;
@@ -21,6 +24,19 @@ private:
 	Vector2f position;
 public:
 	ScaleParameterBar();
+
+    /**
+
+        @param x координата.
+        @param y координата.
+        @param width ширина.
+        @param height высота.
+        @param resource_ указатель на ресурс.
+        @param max_value_ максимальное значение, которое может принимать ресурс.
+        @param t_manager менеджер текстур.
+        @param w_object обьект склада информации о сущностях.
+        @param name название текстуры, сохраненной в менеджере текстур.
+    */
 	ScaleParameterBar(float x, float y, float width, float height, float* resource_, float max_value_, 
 		TextureManager* t_manager, Warehouse* w_object, string name);
 	~ScaleParameterBar();
@@ -28,13 +44,37 @@ public:
 	FloatRect getLocalBounds() override;
 	FloatRect getGlobalBounds() override;
 
+    /** Устанавливает новую позицию.
+
+       @param new_position координаты новой позиции.
+    */
 	void setPosition(Vector2f new_position);
+
+    /** Устанавливает новую координату x для группы. Провоцирует изменение положения всех элементов группы.
+
+       @param x новая координата x.
+    */
 	void setPositionX(float x) override;
+
+    /** Устанавливает новую координату y для группы. Провоцирует изменение положения всех элементов группы.
+
+       @param y новая координата y.
+    */
 	void setPositionY(float y) override;
 
+    /** Сдвигает группу и ее элементы на заданное смещение.
+
+       @param offset_x смещение по x.
+       @param offset_y смещение по y.
+    */
 	void changePosition(float offset_x, float offset_y) override;
 
+    /** Обновляет элемент.
+    */
 	void update() override;
+
+    /** Отображает элемент.
+    */
 	void render() override;
 };
 
